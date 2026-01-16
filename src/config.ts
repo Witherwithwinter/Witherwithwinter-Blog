4c1a5481f77db2aa54bbf3dc0bd20a63de3915b9
OVO Transient <Huyucan060830@outlook.com>
2026-01-16 22:02:16 +0800

    up-comment

--- src/config.ts
+++ src/config.ts
@@ -429,7 +429,7 @@ export const expressiveCodeConfig: ExpressiveCodeConfig = {
 export const commentConfig: CommentConfig = {
 	enable: true, // 启用评论功能。当设置为 false 时，评论组件将不会显示在文章区域。
 	twikoo: {
-		envId: "https://twikoo-for-my-blog.vercel.app",
+		envId: "https://witherwithwinter-twikoo-for-my-blog.hf.space",
 		lang: "zh_CN",
 	},
 };
